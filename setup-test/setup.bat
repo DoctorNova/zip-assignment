87b08b9b535712c6cbe42fd5f379766225162cd9
Sebastian Paas <paas.sebastian@proton.me>
2022-09-11 19:45:45 +0200

    Fix: setup.bat

--- setup-test/setup.bat
+++ setup-test/setup.bat
@@ -1,4 +1,5 @@
-rmdir /S /Q "C:\Users\paass\AppData\Local\Programs\digipen-assignment-zipper"
-robocopy "%~dp0\publish" "C:\Users\paass\AppData\Local\Programs\digipen-assignment-zipper" /E
-SetX PATH "C:\Users\paass\AppData\Local\Programs\digipen-assignment-zipper;%PATH%"
+SET p=%UserProfile%\AppData\Local\Programs\digipen-assignment-zipper
+rmdir /S /Q %p%
+robocopy "%~dp0publish" %p% /E
+SetX PATH "%p%;%PATH%"
 pause
